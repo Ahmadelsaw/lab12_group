b09a0b0ca8cf3d75a1dea4c6505c18a1c10a87b3
Ahmadelsaw <ahmade@gwmail.gwu.edu>
2025-11-05 15:22:35 -0500

    Changed Hello World message and added extra line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello from Ahmad Elsawalhi!");
+        System.out.println("This is an additional line I added for the lab.");
     }
 }
